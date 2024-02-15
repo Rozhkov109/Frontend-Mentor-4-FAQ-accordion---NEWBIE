7c3bbe7752b12ed6a56845921bad16b5dddab194
Rozhkov109 <123330362+Rozhkov109@users.noreply.github.com>
2024-02-15 12:50:05 +0100

    Проект завершён - Сделан JS (криво, но задачи проекта выполняет) - Исправлен HTML и CSS код

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-var button = document.getElementsByClassName("")
+const questions = [
+  document.getElementById("question-1"),
+  document.getElementById("question-2"),
+  document.getElementById("question-3"),
+  document.getElementById("question-4"),
+]
+
+const answers = [
+  document.getElementById("answer-1"),
+  document.getElementById("answer-2"),
+  document.getElementById("answer-3"),
+  document.getElementById("answer-4"),
+]
+
+function answerShow(question, answer) {
+  const image = question.querySelector("img")
+  if (answer.style.display == "block") {
+    answer.style.display = "none"
+    image.src = "/assets/images/icon-plus.svg"
+  } else {
+    answer.style.display = "block"
+    image.src = "/assets/images/icon-minus.svg"
+  }
+}
+
+questions.forEach((question, index) => {
+  question.addEventListener("click", () => answerShow(question, answers[index]))
+})
